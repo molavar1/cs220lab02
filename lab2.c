69f3ca8fabbb75336fc6756b54079ad94c5717aa
molavar1 <42648357+molavar1@users.noreply.github.com>
2020-02-11 12:08:35 -0500

    commit

--- lab2.c
+++ lab2.c
@@ -2,17 +2,35 @@
 
 unsigned long interleave(unsigned int i1, unsigned int i2)
 {
-	unsigned long res;
-	unsigned char *pi1, *pi2, *pi3;
-	pi1 = (unsigned char *) &i1;
-	pi2 = (unsigned char *) &i2;
-	pi3 = (unsigned char *) &res;
+	unsigned long res = 0;
+	char *pi1, *pi2, *pi3;
+	pi1 = (char *) &i1;
+	pi2 = (char *) &i2;
+	pi3 = (char *) &res;
 	int i;
 	for(i = 0; i < 4; i++) {
 		pi3[2*i] = pi2[i];
 		pi3[2*i+1] = pi1[i];
 	}
 	return res;
+}
+unsigned long interleave1(unsigned int i1, unsigned int i2)
+{
+	unsigned long ret = 0;
+	/* We are going to use unsigned long temp variables so we don't shift out of bounds */
+	unsigned long t1 = i1;
+	unsigned long t2 = i2;
+
+	ret |= (t1 & 0xff000000) << 32;
+	ret |= (t2 & 0xff000000) << 24;
+	ret |= (t1 & 0xff0000) << 24;
+	ret |= (t2 & 0xff0000) << 16;
+	ret |= (t1 & 0xff00) << 16;
+	ret |= (t2 & 0xff00) << 8;
+	ret |= (t1 & 0xff) << 8;
+	ret |= t2 & 0xff;
+
+	return ret;
 }
 
 void print_raw_bytes(char *start, unsigned int len)
@@ -41,8 +59,10 @@ int main()
 {
 	unsigned int i1 = 0x12345678;
 	unsigned int i2 = 0xdeadbeef;
-	printf("Ox%x, 0x%x -> 0x%lx\n", i1,i2, interleave(i1,i2));
+	printf("0x%x, 0x%x -> 0x%lx\n", i1,i2, interleave(i1,i2));
 	print_raw_bytes((char *)&i1, sizeof(i1));
 	printf("%x -> %u 1's\n", i2, count_ones(i2));
+	printf("%d\n", count_ones(5));
+	printf("%d\n", count_ones(i1));
 	return 0;
 }
